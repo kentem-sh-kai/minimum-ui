c38b5b91690004820a175adcecfdb0ec8737a7e3
sh-kai <sh-kai@kentem.co.jp>
2025-06-06 16:47:17 +0900

    トーストのボタンのレイアウト変更

--- src/components/demos/BasicToast.tsx
+++ src/components/demos/BasicToast.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Toast from "../ui/Toast";
+import { Button } from "../ui/Button";
 
 const BasicToast = () => {
   const [open, setOpen] = useState(false);
 
   return (
     <div style={{ textAlign: "center" }}>
-      <button onClick={() => setOpen(true)}>トーストを表示</button>
+      <Button onClick={() => setOpen(true)}>トーストを表示</Button>
       <Toast
         open={open}
         message="保存しました！"
